dc39447c4d8b50606d892f5906bd65ecd5e45300
@sverguninna <sverguninna6@gmail.com>
2023-03-26 12:47:37 +0300

    js dz

--- hw-1.js
+++ hw-1.js
@@ -1,16 +1,16 @@
- /* let a = 10;
+ let a = 10;
 alert(a); 
   a = 20;
 alert(a); 
-                /* задание 2 */
+              /* /* /* /* /*   /* задание 2 */
 
-/* let yearRelease = 2008;
-alert(yearRelease)  */
+/let yearRelease = 2008;
+alert(yearRelease)  
                            /* задание 3 */
-/* let nameСreator='Brendan Eich';
-alert(nameСreator); */
+let nameСreator='Brendan Eich';
+alert(nameСreator);
                           /* задание 4 */
-/* let x=10;
+ let x=10;
 let y= 2;
 let sum= x+y;
 let sub= x-y;
@@ -19,41 +19,41 @@ let del = x/y;
 alert(sum);
 alert(sub);
 alert(mul);
-alert(del); */
+alert(del); 
                          /* Задание 5 */
-/* let result= 2**5;
-alert(result);  */
+ let result= 2**5;
+alert(result);  
                         /* задание 6 */
-/* let a = 9;
+ let a = 9;
 let b=2; 
 let result= a%=b;
-alert(result); */
+alert(result); 
                                /* задание 7 = не разобралась с i++ */
-/* let num = 1;
+ let num = 1;
 num += 5;
 num -= 3;
 num *= 7;
 num /= 3;
 num += 1;
 num -= 1;
-alert(num); */ 
+alert(num); 
                           /* задание 8 */
-/* let age=Number(prompt('Сколько вам лет'));
-alert(age); */
+ let age=Number(prompt('Сколько вам лет'));
+alert(age); 
                         /* задание 9 */
-/* const user ={
+const user ={
      namePerson:"Nikol",
      age:22,
      isAdmin: true,
 }
 user.cityResidence = 'kumburgaz'; /* я брала в ковычки city of residence выбивает ошибку.  */
-/* user.age=34;
+ user.age=34;
 delete user.cityResidence;
 console.log(user);
 let info = prompt("Какую информацию хотите узнать о пользователе?");
-alert(user[info]); */
+alert(user[info]); 
 
 /* задание 10 */
 
-/* let name = prompt('Как вас зовут?');
-alert(`Привет ${name }`); */
+ let name = prompt('Как вас зовут?');
+alert(`Привет ${name }`);
